c865a0b8438ca28dcc391ade63d77ea3f81d7628
Maria Gromyko <mazvereva91@mail.ru>
2022-11-15 20:43:29 +0300

    fix: card validator

--- routes/cards.js
+++ routes/cards.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import {
   createCard, findCards, deleteCard, likeCard, dislikeCard,
 } from '../controllers/cards.js';
+import { cardIdValidator } from '../validators/validators.js';
+import { cardBodyValidator } from '../validators/validators.js';
 
 export const cardRoutes = Router();
 
-cardRoutes.post('/cards', createCard);
+cardRoutes.post('/cards', cardBodyValidator, createCard);
 cardRoutes.get('/cards', findCards);
-cardRoutes.delete('/cards/:id', deleteCard);
-cardRoutes.put('/cards/:cardId/likes', likeCard);
-cardRoutes.delete('/cards/:cardId/likes', dislikeCard);
+cardRoutes.delete('/cards/:cardId', cardIdValidator, deleteCard);
+cardRoutes.put('/cards/:cardId/likes', cardIdValidator, likeCard);
+cardRoutes.delete('/cards/:cardId/likes', cardIdValidator, dislikeCard);
--- validators/validators.js
+++ validators/validators.js
@@ -1,13 +1,13 @@
 import { Joi, celebrate } from "celebrate";
 
 // Валидация карточек
-const cardIdValidator = celebrate({
+export const cardIdValidator = celebrate({
   params: Joi.object({
     cardId: Joi.string().hex().length(24).required(),
-  }).required(),
+  }),
 });
 
-const cardBodyValidator = celebrate({
+export const cardBodyValidator = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     link: Joi.string().uri({ scheme: ['http', 'https'] }).required(),
@@ -15,13 +15,13 @@ const cardBodyValidator = celebrate({
 });
 
 // Валидация пользователя
-const userIdValidator = celebrate({
+export const userIdValidator = celebrate({
   params: Joi.object({
     userId: Joi.string().hex().length(24).required(),
   }).required(),
 });
 
-const userBodyValidator = celebrate({
+export const userBodyValidator = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
@@ -31,20 +31,20 @@ const userBodyValidator = celebrate({
   }),
 });
 
-const userAvatarValidator = celebrate({
+export const userAvatarValidator = celebrate({
   params: Joi.object().keys({
     avatar: Joi.string().uri({ scheme: ['http', 'https'] }).required(),
   }),
 });
 
-const userProfileValidator = celebrate({
+export const userProfileValidator = celebrate({
   params: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     about: Joi.string().min(2).max(30).required(),
   }),
 });
 
-const userLoginValidator = celebrate({
+export const userLoginValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
